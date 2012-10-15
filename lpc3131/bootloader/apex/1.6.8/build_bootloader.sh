458a637b4cd6889a5c5cb1a4f10ab6389cb0d15c
root <root@bt.foo.org>
2012-10-15 16:50:37 +0200

    build_bootloader.sh added.

---
+++ lpc3131/bootloader/apex/1.6.8/build_bootloader.sh
@@ -0,0 +1,13 @@
+#! /bin/bash 
+
+#this script compiles the apex bootloader
+
+tar zxvf apex-1.6.8.tar.gz
+cd apex-1.6.8
+
+
+#With the following command the configuration of your apex bootloader takes place. You should set the correct size of your Gnublin-boards RAM via "Platform Setup -> Choose RAM size"
+make menuconfig
+
+#After configuration, run the following command to make the binary file.
+make apex.bin
