97107e9eaebabe9ce4656b0a509566d2a4e07f1e
Niedermayr <benbrenson89@gmail.com>
2012-10-22 11:18:57 +0200

    completion of build_GNUBLIN support package

--- lpc3131/build_GNUBLIN_support_package.sh
+++ lpc3131/build_GNUBLIN_support_package.sh
@@ -15,14 +15,13 @@ distro_version="-max"    				#paste "-min" if you want to build a minimal versio
 root_path=$(pwd)
 toolchain_path=$root_path/toolchain
 cross_compiler_path=$toolchain_path/armv5te/sysroots/i686-oesdk-linux/usr/bin/armv5te-linux-gnueabi
-cross_compiler_path2=/home/brenson/Downloads/armv5te/sysroots/i686-oesdk-linux/usr/bin/armv5te-linux-gnueabi
-cross_compiler_path3=/home/brenson/Downloads/armv5te/sysroots/i686-oesdk-linux/bin/armv5te-linux-gnueabi/
 kernel_version=2.6.33
 kernel_name=linux-$kernel_version-lpc313x
 kernel_path=$root_path/kernel/$kernel_name
 debian_build_path=$root_path/rootfs/debian/debian_install
 debian_installed_files_path=$root_path/rootfs/debian/debian_install/debian_process
 bootloader_install_dir=$root_path/bootloader/apex/1.6.8
+logfile_build=$root_path/install.log
 
 
 ##########################################################
@@ -60,45 +59,46 @@ fi
 
 
 #############################################
-# 1st Stage:Build toolchain and bootloader  #
+# 1st Stage:Build toolchain                 #
 #############################################
+
 source $root_path/toolchain/build_toolchain.sh
+
+#############################################
+# 2nd Stage:Build bootlader                 #
+#############################################
 source $root_path/kernel/set.sh
-
-
-
-#export PATH=$cross_compiler_path2:$cross_compiler_path3:$PATH
-#export ARCH=arm
-#export CROSS_COMPILE=arm-linux-gnueabi-
-
-echo "$ARCH"
-echo "$CROSS_COMPILE"
-echo "$PATH"
-
 source $root_path/bootloader/apex/1.6.8/build_bootloader.sh
 
 
 
 ######################################
-# 2nd Stage: Kernel and rootfs build # 
+# 3rd Stage: Kernel build		     # 
 ######################################
 
 # Including settings through an additional file
 source $root_path/rootfs/debian/debian_install/general_settings.sh	"$distro_version"	 
 source $root_path/kernel/build_kernel.sh
 
-
-#exit 0
-
 # Move set.sh file into the kernel
 mv $root_path/kernel/set.sh $kernel_path
-
 source $root_path/rootfs/debian/debian_install/build_debian_system.sh 
 
 
-exit 0
+
+
+
+
+######################################
+# 4th Stage: Rootfs completion  add  # 
+######################################
 # 2.Copy some important support files into the rootfs
 #   (e.g. example applications(im home ordner),debian packages---->add to packages list???)
+
+
+######################################
+# 5th Stage: Rootfs completion remove# 
+######################################
 
 ### Uninstall unimportant files out of the rootfs.
 ### (Avahi, samba, Documentation, ARCH, )
@@ -106,17 +106,21 @@ exit 0
 
 
 
-#--->pre-compiled (examples??)
 
-# 3.Build support folder
+######################################
+# 6th Stage: Built Support package   # 
+######################################
+# --> Build support folder
 #   (e.g. for how-to files,examles,source_code)
+# --> Alles wenn möglich für den Gnblin installer komform machen!
 
 
-# 4. User rights im Wiki nicht hier!!!
 
-
-# 5. Alles wenn möglich für den Gnblin installer komform machen!
-# 6. Auf die CD einen Ordner mit einem Abbild dieser Struktur + alles fertig ordner(für die Oma)
+######################################
+# 7th Stage: Built Support package   # 
+######################################
+# --> User rights im Wiki nicht hier!!!
+# --> Auf die CD einen Ordner mit einem Abbild dieser Struktur + alles fertig ordner(für die Oma)
 
 
 
--- lpc3131/toolchain/build_toolchain.sh
+++ lpc3131/toolchain/build_toolchain.sh
@@ -4,62 +4,63 @@
 
 #Check if there is a previuos installation of eldk on your computer
 eldk_version=$(ls /opt/ | grep eldk)
-
+build_time="$(date '+%D %H:%M:%S') ->"
 
 #check if the output of ls | grep is not empty
 if [ ${#eldk_version} -lt  3 ]
 then
 
-eldk_version="NotFound"
-
+	eldk_version="NotFound"
+	
 fi
 
 
 if [ -d "/opt/$eldk_version" ]
 then
 
-#Create only a softlink to your installation
-ln -s /opt/$eldk_version/armv5te $toolchain_path/armv5te 
-
+	#Create only a softlink to your installation
+	ln -s /opt/$eldk_version/armv5te $toolchain_path/armv5te 
+	echo "$build_time A link to installed toolchain created in $toolchain_path." >> $logfile_build
 
 elif [ -d "$toolchain_path/armv5te" ]
 then
-#echo if you have already installed the toolchain in your toolchain directory
-echo "Toolchain already installed!"
-
+	#echo if you have already installed the toolchain in your toolchain directory
+	echo "Toolchain already installed!"
+	echo "$build_time Toolchain is already installed on your PC." >> $logfile_build 
 
 else
-#Install the toolchain
+	#Install the toolchain
 
-if [ ! -d "$root_path/Downloads" ]
-then
-    mkdir $root_path/Downloads
-fi
+	if [ ! -d "$root_path/Downloads" ]
+	then
+    	mkdir $root_path/Downloads
+	fi
 
     cd $root_path/Downloads 
 
-#Downloading the ELDK 5.0 iso if you dont have it
-if [ ! -f "$root_path/Downloads/armv5te-qte-5.0.iso" ]
-then
-    wget ftp://ftp.denx.de/pub/eldk/5.0/iso/armv5te-qte-5.0.iso 
-fi
+	#Downloading the ELDK 5.0 iso if you dont have it
+	if [ ! -f "$root_path/Downloads/armv5te-qte-5.0.iso" ]
+	then
+    	wget ftp://ftp.denx.de/pub/eldk/5.0/iso/armv5te-qte-5.0.iso || echo "$build_time Error while downloading the toolchain." >> $logfile_build 
+	fi
 
-    cd /media 
+   	cd /media 
     mkdir eldk-iso 
 
-# Mount the iso file
+	# Mount the iso file
     mount -o loop $root_path/Downloads/armv5te-qte-5.0.iso /media/eldk-iso 
     cd /media/eldk-iso
  
-#Start the installation
+	#Start the installation
     ./install.sh -s -i qte armv5te 
 
-if [ ! -h "$toolchain_path/armv5te" ]
-then
-#Create a softlink after installation
-    ln -s /opt/$eldk_version/armv5te $toolchain_path/armv5te
-fi
-#Remove temp directories of the installation
+	if [ ! -h "$toolchain_path/armv5te" ]
+	then
+		eldk_version=$(ls /opt/ | grep eldk)
+		#Create a softlink after installation
+    	ln -s /opt/$eldk_version/armv5te $toolchain_path/armv5te
+	fi
+	#Remove temp directories of the installation
     cd $root_path 
     umount /media/eldk-iso 
     rmdir /media/eldk-iso  
@@ -72,15 +73,14 @@ fi
 #Creating the source-path-script, also known as set.sh
 
 if [ ! -e "$root_path/kernel/set.sh" ]
-
 then
-#Create the set.sh file
+	#Create the set.sh file
 
-touch $root_path/kernel/set.sh
-chmod +x $root_path/kernel/set.sh
+	touch $root_path/kernel/set.sh
+	chmod +x $root_path/kernel/set.sh
 
 
-echo     "	 P1=$toolchain_path/armv5te/sysroots/i686-oesdk-linux/usr/bin/armv5te-linux-gnueabi/
+	echo     "	 P1=$toolchain_path/armv5te/sysroots/i686-oesdk-linux/usr/bin/armv5te-linux-gnueabi/
 	 P2=$toolchain_path/armv5te/sysroots/i686-oesdk-linux/bin/armv5te-linux-gnueabi/
 
 	 export ARCH=arm 
